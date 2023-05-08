3bd0ddcc1ed04ed6a9ba7960133367f32966c31e
Ravi Pai <119127289+Oauth-Celestial@users.noreply.github.com>
2023-05-08 20:23:48 +0530

    Resolved null issue

--- Flutter/InternetConnectivityCheck/CheckConnectivityContainer.dart
+++ Flutter/InternetConnectivityCheck/CheckConnectivityContainer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-import 'package:picsizer/Services/NetworkService/ConnectionHelper.dart';
 
 class ConnectivityConatiner extends StatelessWidget {
   final Widget child;
@@ -15,12 +14,32 @@ class ConnectivityConatiner extends StatelessWidget {
         StreamBuilder<NetworkStatus>(
             stream: ConnectionHelper().controller.stream,
             builder: (context, snapshot) {
+              if (snapshot.data == null) {
+                return FutureBuilder<ConnectivityResult>(
+                    future: Connectivity().checkConnectivity(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        if (snapshot.data == (ConnectivityResult.none)) {
+                          return Container(
+                            color: Colors.transparent,
+                            alignment: Alignment.center,
+                            child: LottieBuilder.asset("assets/wifi.json"),
+                          );
+                        } else {
+                          return Container();
+                        }
+                      }
+                      return Container();
+                    });
+              }
+
               return Container(
                 child: snapshot.data == NetworkStatus.online
                     ? Visibility(visible: false, child: Container())
 
                     // Your No Internet widget
-                    : Align(
+                    : Container(
+                        color: Colors.transparent,
                         alignment: Alignment.center,
                         child: LottieBuilder.asset("assets/wifi.json"),
                       ),
@@ -30,3 +49,4 @@ class ConnectivityConatiner extends StatelessWidget {
     );
   }
 }
+
